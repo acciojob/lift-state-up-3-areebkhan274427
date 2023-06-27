6b921d2184d1883c014127d098f4011385674869
Areeb <areebkhan274427@gmail.com>
2023-06-28 01:28:17 +0530

    Changes 2

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -4,7 +4,7 @@ import Child2 from './Child2';
 function Parent() {
     const [option,setOption] = useState('')
   return (
-    <div>
+    <div className='parent'>
         <h1>Parent Component</h1>
         <Child1 optionChange={setOption}/>
         <Child2 optionChange={setOption}/>
